408cc27c3251fbcf7417d82f6d4a423caa5fde8f
Nahid-07 <nahidasif1998@gmail.com>
2022-11-26 16:00:48 +0600

    load data by category id

--- index.js
+++ index.js
@@ -18,17 +18,25 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const userCollection = client.db('assignment-12-DB').collection('user')
-        const categoryCollections = client.db('assignment-12-DB').collection('categoriesOfProduct')
+        const categoryCollections = client.db('assignment-12-DB').collection('categoriesOfProduct');
+        const productCollections = client.db('assignment-12-DB').collection('products')
         const buyerCollections = client.db('assignment-12-DB').collection('buyer')
+        
         app.get('/category', async(req,res)=>{
             const query = {}
             const category = await categoryCollections.find(query).toArray()
             res.send(category)
         })
+        app.post('/product',async(req,res)=>{
+            const product = req.body
+            const result = await productCollections.insertOne(product);
+            res.send(result)
+        })
         app.get('/category/:id', async(req,res)=>{
-            const id = req.params.id
-            const query = {_id : ObjectId(id)}
-            const result = await categoryCollections.findOne(query)
+            const id = req.params.id;
+            // const query = {i}
+            console.log(id);
+            const result = await productCollections.find({id}).toArray()
             res.send(result)
         })
         app.post('/users', async(req,res)=>{
@@ -36,10 +44,21 @@ async function run(){
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
+        app.get('/users',async(req,res)=>{
+            const query = {}
+            const result = await userCollection.find(query).toArray();
+            res.send(result)   
+
+        })
         app.post('/buyer', async(req,res)=>{
             const buyer = req.body;
             const result = await buyerCollections.insertOne(buyer);
             res.send(result)
+        })
+        app.get('/buyer', async(req,res)=>{
+            const query ={};
+            const buyers = await buyerCollections.find(query).toArray()
+            res.send(buyers)
         })
     }
     finally{
